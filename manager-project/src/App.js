7fd25f1c04f7641ce4f9824beb5aff67fa0f8694
Akash Dhanwar <akash.dhanwar112@gmail.com>
2023-10-24 14:34:44 +0530

    First Commit

---
+++ manager-project/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Test from "./Test/Test";
+
+function App() {
+  return (
+    <div className="App">
+      <Test />
+      <p>Lec-5</p>
+    </div>
+  );
+}
+
+export default App;
